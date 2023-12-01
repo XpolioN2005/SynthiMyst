1586b27973be940ff45d3016790450ce060a4f3a
XpolioN2005 <iamsupriya1234+github@gmail.com>
2023-12-01 16:01:08 +0530

    responsive navbar part final

--- synthimyst-web/script.js
+++ synthimyst-web/script.js
@@ -1,7 +1,23 @@
 const navBtn = document.getElementById("menu")
+const navclose = document.getElementById("nav-close")
+const menu = document.getElementById("other-navbar");
 
 navBtn.addEventListener("click", function () {
-    if (!navBtn.className.match("hidden")) {
-        navBtn.classList.toggle("hidden")
+    if (menu.className.match("hidden")) {
+        menu.classList.remove("hidden");
+        navBtn.classList.toggle("hidden");
+        menu.classList.add("visible")
+    }
+})
+
+navclose.addEventListener("click", function () {
+    if (menu.className.match("visible")) {
+        menu.style.opacity = 1
+        menu.classList.remove("visible");
+        menu.classList.add("hidden")
+
+        setTimeout(function(){
+            navBtn.classList.toggle("hidden")
+        }, 500)
     }
 })
